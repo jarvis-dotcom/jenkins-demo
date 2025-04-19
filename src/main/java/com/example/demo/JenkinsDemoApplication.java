e0f67af1d7bea1a07aa56b8fd0197fb8a05fcad1
hariharan <tcehariharan@gmail.com>
2025-04-20 01:37:46 +0530

    Added basic code

--- src/main/java/com/example/demo/JenkinsDemoApplication.java
+++ src/main/java/com/example/demo/JenkinsDemoApplication.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsDemoApplication {
 
+	@GetMapping("/get")
+	public String message() {
+		return "welcome to jenkins demo";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
--- src/test/java/com/example/demo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/example/demo/JenkinsDemoApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class JenkinsDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+        Assertions.assertTrue(true);
 	}
 
 }
